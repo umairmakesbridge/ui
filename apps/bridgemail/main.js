a742b15cf41f5d28f5c7f78a436bea8edd1fbc07
Umair Shahid <umair.makesbridge@gmail.com>
2014-02-25 22:57:25 +0500

    V 1.5

--- apps/bridgemail/main.js
+++ apps/bridgemail/main.js
@@ -3,7 +3,7 @@
     require.config({  
        deps: ['main'], 
        waitSeconds:300,
-       urlArgs: "bust=1.4" ,
+       urlArgs: "bust=1.5" ,
        paths:{
            jquery:'../shared/libs/jquery',
            underscore:'../shared/libs/underscore',
