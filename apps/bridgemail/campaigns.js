b725b08c40588b39a1229c8af5ef561192c2f9f6
Abdullah Tariq <abdullah@borialas.com>
2014-03-07 21:23:29 +0500

    Deployment changes

--- apps/bridgemail/campaigns.js
+++ apps/bridgemail/campaigns.js
@@ -269,8 +269,10 @@ function (bmsgrid,jqhighlight,jsearchcontrol,template,bmsfilters,_daterangepicke
                                         camp_obj.$el.find("#area_copy_campaign .bmsgrid").remove();
                                         camp_obj.app.removeCache("campaigns");
                                         camp_obj.getallcampaigns();
-                                        var wp_id = $("#wstabs li[workspace_id=campaign_"+camp_id+"]").attr('id').split("_")[2];
-                                        $("#wp_li_"+wp_id+",#workspace_"+wp_id).remove();
+                                         if($("#wstabs li[workspace_id=campaign_"+camp_id+"]").length){
+                                            var wp_id = $("#wstabs li[workspace_id=campaign_"+camp_id+"]").attr('id').split("_")[2];
+                                            $("#wp_li_"+wp_id+",#workspace_"+wp_id).remove();
+                                       }
                                 }
                                 else{
                                              camp_obj.app.showAlert(del_camp_json[1],camp_obj.$el.parents(".ws-content.active"));							
