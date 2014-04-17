0090d9b5829cb8b94b9a620d14b5a0fd53bd3786
Umair Shahid <umair.makesbridge@gmail.com>
2014-04-17 14:25:41 +0500

    tags area fix

--- apps/bridgemail/main.js
+++ apps/bridgemail/main.js
@@ -3,7 +3,7 @@
     require.config({  
        deps: ['main'], 
        waitSeconds:300,
-       urlArgs: "bust=1.18" ,
+       urlArgs: "bust=1.19" ,
        paths:{
            jquery:'../shared/libs/jquery',
            underscore:'../shared/libs/underscore',
