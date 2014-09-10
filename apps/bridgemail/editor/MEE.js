bb064f1c3e1f1084127637bcaead94f5ff63312a
Umair Shahid <umair.makesbridge@gmail.com>
2014-09-10 21:42:29 +0500

    decoded value code

--- apps/bridgemail/editor/MEE.js
+++ apps/bridgemail/editor/MEE.js
@@ -1590,7 +1590,7 @@ define(['jquery','backbone', 'underscore', 'text!editor/html/MEE.html','jquery-u
                                                                 "a87ff679a2f3e71d9181a67b7542122c":"columnImageWithHeading",
                                                                 "e4da3b7fbbce2345d7772b0674a318d5":"BannerPlusText",
                                                                 "1679091c5a880faf6fb5e6087eb1b2dc":"Button",
-                                                                "8f14e45fceea167a5a36dedd4bea2543":"footer",
+                                                                "4e732ced3463d06de0ca9a15b6153677":"footer",
                                                                 "c9f0f895fb98ab9159f51fd0297e236d":"FooterPlusLogo",
                                                                 "45c48cce2e2d7fbdea1afc51c7c6ad26":"FooterPlusLogo",
                                                                 "d3d9446802a44259755d38e6d163e820":"ImageTextbutton",
