f6c92762aa5131321fbbab7059ae3f27b6406568
Umair Shahid <umair.makesbridge@gmail.com>
2014-10-05 16:23:46 +0500

    Easy editor changes

--- apps/bridgemail/main.js
+++ apps/bridgemail/main.js
@@ -3,7 +3,7 @@
     require.config({  
        deps: ['main'], 
        waitSeconds:400,
-       urlArgs: "bust=1.391" ,
+       urlArgs: "bust=1.392" ,
        paths:{
            jquery:'shared/libs/jquery',
            underscore:'shared/libs/underscore',
