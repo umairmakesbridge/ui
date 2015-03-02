9dcd321cbbdd8f37acad9315fbc181533b8544e6
Umair Shahid <umair.makesbridge@gmail.com>
2015-03-03 01:46:36 +0500

    sale rep selection file missed

---
+++ apps/bridgemail/account/salesrep_selection_row.js
@@ -0,0 +1,51 @@
+define(['text!account/html/salesrep_selection_row.html'],
+function (template) {
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //
+        // Salesrep selection View to show on add update sub account dialog
+        //
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        'use strict';
+        return Backbone.View.extend({            
+            tagName: 'tr',
+            className:'erow',
+            /**
+             * Attach events on elements in view.
+            */
+            events: {              
+            },
+            /**
+             * Initialize view - backbone
+            */
+            initialize: function () {                    
+                    this.app = this.options.sub.app;
+                    this.template = _.template(template);				                                                      
+                    this.render();                    
+            },
+            /**
+             * Render view on page.
+            */
+            render: function () {                                    
+                this.$el.html(this.template({
+                    model: this.model
+                }));                
+                this.initControls();                 
+            },
+            /**
+             * Render Row view on page.
+            */
+            renderRow:function(){
+                
+            },
+            /**
+             * Initializing all controls here which need to show in view.
+            */
+            initControls:function(){
+                this.$('input.checkpanel').iCheck({
+                    checkboxClass: 'checkpanelinput',
+                    insert: '<div class="icheck_line-icon" style="margin:18px 0 0 10px"></div>'
+                });
+                this.$(".showtooltip").tooltip({'placement':'bottom',delay: { show: 0, hide:0 },animation:false});
+            }
+        });
+});
