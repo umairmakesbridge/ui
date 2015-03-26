843831cfd686ada68f20abf56ae7bdf952f7c6b1
Umair Shahid <umair.makesbridge@gmail.com>
2015-03-26 21:35:18 +0500

    custom charts tile placed

--- apps/bridgemail/main.js
+++ apps/bridgemail/main.js
@@ -3,7 +3,7 @@
     require.config({  
        deps: ['main'], 
        waitSeconds:400,
-       urlArgs: "bust=1.462_2.1" ,
+       urlArgs: "bust=1.462_3" ,
        paths:{
            jquery:'shared/libs/jquery',
            underscore:'shared/libs/underscore',
