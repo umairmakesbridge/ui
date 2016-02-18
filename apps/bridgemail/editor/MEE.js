14145c0494b0d0bb01a7fbcc3f769d02af0398a0
abdullah tariq <faabdullahtariq@gmail.com>
2016-02-18 17:26:27 +0500

    Text change for the Button

--- apps/bridgemail/editor/MEE.js
+++ apps/bridgemail/editor/MEE.js
@@ -1035,7 +1035,7 @@ define(['jquery', 'backbone', 'underscore', 'text!editor/html/MEE.html', 'editor
                                             headerIcon: 'actionScripicon',
                                             buttons: {
                                                 saveBtn: {
-                                                    text: 'Add Script'
+                                                    text: 'Save'
                                                 }
                                             }
                                         });
