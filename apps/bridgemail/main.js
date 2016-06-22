eca54a326c35d6b3f69ef7c2ca6037e6799d0893
coldflame <umair@makesbridge.com>
2016-06-22 18:19:56 +0500

    version change

--- apps/bridgemail/main.js
+++ apps/bridgemail/main.js
@@ -3,7 +3,7 @@
     require.config({  
        deps: ['main'], 
        waitSeconds:400,       
-       urlArgs: "bust=1.472_81" ,       
+       urlArgs: "bust=1.472_8" ,       
        paths:{
            jquery:'shared/libs/jquery',
            underscore:'shared/libs/underscore',
