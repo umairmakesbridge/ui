2477b72cb73baf5aceb9ec929adf98c8da4c78a9
umairmakesbridge <umair@makesbridge.com>
2016-10-28 17:53:16 +0500

    nuturetracks added to add row bar  in reports

--- apps/bridgemail/main.js
+++ apps/bridgemail/main.js
@@ -3,7 +3,7 @@
     require.config({  
        deps: ['main'], 
        waitSeconds:400,       
-       urlArgs: "bust=1.472_87" ,       
+       urlArgs: "bust=1.472_88" ,       
        paths:{
            jquery:'shared/libs/jquery',
            underscore:'shared/libs/underscore',
