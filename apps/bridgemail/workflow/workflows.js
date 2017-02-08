2a21c649537bdea384b4f12a62950505074a4f82
abdullahtariq <faabdullahtariq@gmail.com>
2017-02-08 15:45:46 +0500

    copy workflow and icon images for image toolbar see

--- apps/bridgemail/workflow/workflows.js
+++ apps/bridgemail/workflow/workflows.js
@@ -134,6 +134,89 @@ define(['text!workflow/html/workflows.html', 'jquery.searchcontrol','jquery.high
                         this.$(".workflowiframe").attr("src","about:blank").css("height","400px");
                     }
                     this.app.showLoading(msg?msg:"Loading Workflows...",this.$el);    
+                },
+                copyWFDialog : function(id,title,isWfEdit){
+                    var dialog_title = "Copy Workflow";
+                    var dialog = this.app.showDialog({title: dialog_title,
+                        css: {"width": "600px", "margin-left": "-300px"},
+                        bodyCss: {"min-height": "260px"},
+                        headerIcon: 'copycamp',
+                        buttons: {saveBtn: {text: 'Copy Workflow'}}
+                    });
+                    
+                    this.app.showLoading("Loading...", dialog.getBody());
+                    
+                    //require(["campaigns/copycampaign"], _.bind(function (copycampaignPage) {
+                        var dialogHtml = '<div class="model_form" style="display: block;" id="">';
+                            dialogHtml += '<div class="copy_campbox">'
+                            dialogHtml += '<div class="">'
+                            dialogHtml += '<h2><span>'+title+'</span> </h2>'
+                            dialogHtml += '<div class="tagscont">'
+                            dialogHtml += '</div></div>'
+                            dialogHtml += '<div class="img"><img alt="" data-src="holder.js" src="img/copycampaign.png" width="160" height="150"></div></div>';
+                            dialogHtml += '<div class="row-fluid">'
+                            dialogHtml += '<div class="span12">'
+                            dialogHtml += '<div class="row campname-container">'
+                            dialogHtml += '<label>New Workflow Name</label>'
+                            dialogHtml += '<div class="input-append adv_dd sort-options">'
+                            dialogHtml += '<div class="inputcont">'
+                            dialogHtml += '<input id="camp_name" type="text" placeholder="Enter workflow name here" class="header-info textfield">'
+                            dialogHtml += '</div></div></div></div></div><div class="clearfix"></div></div>';
+                          
+                        dialog.getBody().html(dialogHtml);
+                        dialog.getBody().find('#camp_name').focus();
+                        dialog.getBody().find("input#camp_name").keypress(_.bind(function (e) {
+                                            if (e.keyCode == 13) {
+                                                this.copyWorkFlow(id,dialog,isWfEdit);
+                                            }
+                                        },this))
+                         
+                        dialog.saveCallBack(_.bind(this.copyWorkFlow, this,id,dialog,isWfEdit));
+                },
+                copyWorkFlow : function(id,dialogObj,isWfEdit){
+                    
+                   var wf_name = dialogObj.getBody().find('#camp_name').val();
+                   var _this = this;
+                    if( wf_name == ''){						
+                                this.app.showError({
+                                        control:dialogObj.getBody().find('.campname-container'),
+                                        message:"Workflow name cannot be empty"
+                                });
+                        }
+                        else{
+                             
+                             var admin = false;
+                             if(_this.app.get("user").accountType == "S"){
+                                 admin = true
+                             }
+                            _this.app.showLoading("Copying Workflow...",dialogObj.getBody()); 
+                            $.post( "/pms/input/workflow/saveWorkflowData.jsp?BMS_REQ_TK="+this.app.get('bms_token'), { 
+                                    actionType: "copy", 
+                                    workflowId: id,
+                                    name: wf_name,
+                                    admin : admin // Need to ask about this flag
+                                })
+                                .done(function( data ) {
+                                  //alert( "Data Loaded: " + data ); // Server not responding anything thought it creates the workflow
+                                 if(data[0]=="err"){
+                                     _this.app.showAlert(data[1], $("body"), {fixed: true});
+                                 }else{
+                                        if(isWfEdit){
+                                          _this.$el.find('iframe').attr('src','/pms/trigger/workflow.jsp?BMS_REQ_TK='+_this.app.get('bms_token')+'&fromNewUI=true&workflowId='+data[1]);
+                                       }else{
+                                           _this.$el.find('.refresh_btn').trigger('click');
+
+                                       }
+                                        dialogObj.$el.find('.btn-close').trigger('click');
+                                 }
+                                    
+                                  _this.app.showLoading(false,dialogObj.getBody());
+                                  
+                                  
+                                });
+                        }
+                    
+                    
                 }
                 
 
