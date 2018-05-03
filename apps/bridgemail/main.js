cc74a059df96cd8e6b0003d40aa669b683bedbca
Umair <umair.makesbridge@gmail.com>
2018-05-03 14:49:30 +0500

    version change for test

--- apps/bridgemail/main.js
+++ apps/bridgemail/main.js
@@ -3,7 +3,7 @@
     require.config({  
        deps: ['main'], 
        waitSeconds:400,       
-       urlArgs: "bust=1.472_9199" ,       
+       urlArgs: "bust=1.472_922" ,       
        paths:{
            jquery:'shared/libs/jquery',
            underscore:'shared/libs/underscore',
